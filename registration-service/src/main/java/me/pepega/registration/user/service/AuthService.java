9c65327a69c5d997e9a01fe8bcfad406aa7c8efc
pepegazxc <werdewrs555@gmail.com>
2026-07-24 21:47:29 +0300

    feature(registration) : Create AuthService class

---
+++ registration-service/src/main/java/me/pepega/registration/user/service/AuthService.java
@@ -0,0 +1,10 @@
+package me.pepega.registration.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AuthService {
+    //TO DO: Write registration and authentication logic
+}
