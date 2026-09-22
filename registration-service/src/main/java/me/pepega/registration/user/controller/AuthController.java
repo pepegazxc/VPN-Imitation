120f31fc3479da3fe70c8228d1deac079dc7bdee
pepegazxc <werdewrs555@gmail.com>
2026-09-22 19:43:10 +0300

    refactor(registration) : Refactor endpoint name in the controller
    Refactor endpoint name in the AuthController and add RequestMapping annotation to the controller

--- registration-service/src/main/java/me/pepega/registration/user/controller/AuthController.java
+++ registration-service/src/main/java/me/pepega/registration/user/controller/AuthController.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/auth")
+@RestController
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
