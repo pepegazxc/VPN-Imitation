02e0792a5c879872327396f64613e10edb89e5ca
pepegazxc <werdewrs555@gmail.com>
2026-09-22 19:35:12 +0300

    feat(registration) : Add security configuration
     Add class SecurityConfig with basic configuration of SecurityFilterChain and PasswordEncoder

--- registration-service/src/main/java/me/pepega/registration/user/security/SecurityConfig.java
+++ registration-service/src/main/java/me/pepega/registration/user/security/SecurityConfig.java
@@ -1,8 +1,44 @@
 package me.pepega.registration.user.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    //TO DO : Add security  and password encoder configuration
+
+    @Bean
+    SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http.
+                csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/registration").permitAll()
+                                .anyRequest().authenticated()
+                )
+                ;
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder encoder(){
+        return new Argon2PasswordEncoder(
+                16,
+                32,
+                1,
+                65536,
+                3
+        );
+    }
 }
